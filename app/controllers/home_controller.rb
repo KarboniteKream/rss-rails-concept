538817885888bfd6540a025c9cf4d6dd98e65714
Klemen Košir <klemen.kosir@kream.io>
2015-01-20 22:05:53 +0100

    Updated home_controller.rb.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,24 @@ class HomeController < ApplicationController
   helper_method :folderSubscriptions, :changeFeed
 
   def feed
-	@feed = Feed.find(params[:id])
+	@feedID = params[:id].to_i
 
-	@articles = Article.joins("JOIN feeds ON feeds.id = articles.feed_id").joins("JOIN unreads ON articles.id = unreads.article_id").joins("JOIN users ON users.id = unreads.user_id").where("articles.feed_id = ? AND users.id = ?", params[:id], 1).order("articles.date DESC").select("articles.id, articles.title, articles.url, articles.author, articles.date, articles.content")
+	if(@feedID == nil || @feedID == 0)
+		@feedID = 1
+	elsif(@feedID < 0)
+		case @feedID
+		when -1
+			redirect_to action: "home"
+		when -2
+		when -3
+		when -4
+		end
+		return
+	end
+
+	@feed = Feed.find(@feedID)
+
+	@articles = Article.joins("JOIN feeds ON feeds.id = articles.feed_id").joins("JOIN unreads ON articles.id = unreads.article_id").joins("JOIN users ON users.id = unreads.user_id").where("articles.feed_id = ? AND users.id = ?", @feedID, 1).order("articles.date DESC").select("articles.id, articles.title, articles.url, articles.author, articles.date, articles.content")
 
 	@nullSubscriptions = Subscription.where("user_id = ? AND (folder IS NULL OR folder = '')", 1).joins("JOIN feeds ON feed_id = feeds.id").order("feeds.name ASC").select("feeds.name, feed_id, feeds.icon")
 
